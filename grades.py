523e2678676d2693827a654ad8a06bc792e23f65
Your Name <you@example.com>
2021-05-04 19:37:52 +0600

    first create of classes

---
+++ grades.py
@@ -0,0 +1,39 @@
+class Anime:
+    time = 22.24
+    main_characters = 'Toboyashi, Girou'
+    antogonist = 'None'
+    def play_anime(self):
+        print('Озвучено  для ознакамительного просмотра командой AniLibria')
+    def pause(self):
+        print('Время спать')
+
+anime_pl = Anime()
+anime_p = Anime()
+
+print(anime_pl.play_anime())
+print(anime_p.pause())
+
+
+
+# # Создаем класс Car
+# class Car:
+
+#     # создаем атрибуты класса
+#     name = "c200"
+#     make = "mercedez"
+#     model = 2008
+
+#     # создаем методы класса
+#     def start(self):
+#         print ("Заводим двигатель")
+
+#     def stop(self):
+#         print ("Отключаем двигатель")
+        
+
+
+# # Создаем объект класса Car под названием car_a
+# car_a = Car()
+ 
+# # Создаем объект класса Car под названием car_b
+# car_b = Car()
